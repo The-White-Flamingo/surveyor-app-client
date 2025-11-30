15f5d99493c97b7ac25c691e3db20a9f688ebc44
Gaius_Omega <97457011+gaijuliuscaesar@users.noreply.github.com>
2025-11-30 22:23:00 +0000

    Update API endpoint for sign-in request

--- app/login/page.js
+++ app/login/page.js
@@ -18,7 +18,7 @@ export default function LoginPage() {
     setErrorMsg("");
 
     try {
-      const res = await fetch("http://localhost:5000/api/signin", {
+      const res = await fetch("https://an-site-solutions-backend.onrender.com/api/signin", {
         method: "POST",
         credentials: "include",
         headers: { "Content-Type": "application/json" },
