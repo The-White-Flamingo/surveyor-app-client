00f313360ff678f07aacda1010cca8cfba828861
Michael Agbeyebiawo <agbeyebiawomicheal@gmail.com>
2025-12-14 00:31:49 +0000

    removed used fonts

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,16 @@
-import { Geist, Geist_Mono } from "next/font/google";
+// import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import AuthProvider from "./providers/AuthProvider";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
 export const metadata = {
   title: "Create Next App",
@@ -21,7 +21,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased max-h-screen`}
+        // className={`${geistSans.variable} ${geistMono.variable} antialiased max-h-screen`}
+        className="antialiased max-h-screen"
       >
         <AuthProvider>
           {children}
